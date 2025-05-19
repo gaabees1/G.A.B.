c1d4dd8205d0cdbfa2c0de9e094ba7bf3fb69f40
gaabees1 <bielhpc2008@outlook.com>
2025-05-19 17:17:30 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# G.A.B.
+# ProjetoDelivery
---
+++ js/app.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    const menu = document.querySelector('.hamburger-menu');
+    menu.classList.toggle('open'); // Adiciona ou remove a classe 'open' para mostrar/ocultar o menu
+  }
